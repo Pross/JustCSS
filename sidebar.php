144e8906606d2123e252def1ea2eb2d3447d1749
Pross <pross@pross.org.uk>
2010-12-08 22:01:30 +0000

    fixed smileys fixed sidebars

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
  */
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
-			<?php if ( ! dynamic_sidebar( 'sidebar' ) ) : ?>
+			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 				<aside id="search" class="widget widget_search">
 					<?php get_search_form(); ?>
 				</aside>
