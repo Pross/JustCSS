64153527b837bb345a491a1affa66e7bef496a66
Pross <pross@pross.org.uk>
2011-01-26 19:37:34 +0000

    selectable google fonts with preview! Added filter to easily add fonts, with demo in functions.php

--- functions.php
+++ functions.php
@@ -4,8 +4,16 @@ require_once( 'functions/justcss_functions.php' );
 require_once( 'functions/cpanel.php' );
 require_once( 'functions/filters.php' );
 
+// Add a custom footer.
 add_filter('jcss_footer_filter', 'footer_add_version');
 function footer_add_version( $footer ) {
 	$footer .= '<div class="justcss_footer_class"><span class="entry-meta"><a href="http://www.pross.org.uk/themes/">JustCSS</a></span> v' . JCSS_VERSION . ' by Pross</div>';
 	return $footer;
+}
+
+// Lets add a font to the font list in the JustCSS contol page.
+add_filter( 'jcss_google_fonts', 'add_my_font' );
+function add_my_font( $fonts ) {
+	 $fonts[] = 'Coming Soon';
+	  return $fonts;
 }
--- functions/cpanel.php
+++ functions/cpanel.php
@@ -1,27 +1,39 @@
 <?php
-add_action('admin_init', 'jcss_options_init_fn' );
-add_action('admin_menu', 'jcss_options_add_page_fn');
-add_action('init', 'add_defaults_fn');
+add_action( 'admin_init', 'jcss_options_init_fn' );
+add_action( 'admin_menu', 'jcss_options_add_page_fn' );
+add_action( 'init', 'add_defaults_fn' );
 function jcss_options_init_fn(){
 	wp_register_script( 'jscolor', get_template_directory_uri() . '/functions/jscolor/jscolor.js' );
 	wp_enqueue_script( 'jscolor' );
-	register_setting('jcss_options', 'jcss_options', 'jcss_options_validate' );
-	add_settings_section('main_section', 'Main Settings', 'section_text_fn', __FILE__);
-	add_settings_field('main_font', 'Google Font', 'setting_font_dropdown_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_nav', 'Navbar Colour', 'setting_nav_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_nav_col', 'Select menu color scheme', 'setting_nav_dropdown_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_widget', 'Widget Colour', 'setting_widget_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_sticky', 'Sticky Colour', 'setting_sticky_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_bpo_radio', 'Enable bypostauthor css', 'setting_bpo_enable_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_bypostauthor', 'Colour for .bypostauthor', 'setting_bypostauthor_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_comment_even', 'Comments even', 'setting_comment_even_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_comment_odd', 'Comments odd', 'setting_comment_odd_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_aside', 'Aside Colour', 'setting_aside_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_corners', 'Corner radius', 'setting_corner_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_brackets', 'Enable {} around blog title', 'setting_brackets_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_mainfont', 'Primary font colour', 'setting_mainfont_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_width', 'Theme width px', 'setting_width_fn', __FILE__, 'main_section');
-	add_settings_field('jcss_reset', 'Restore Defaults?', 'setting_reset_fn', __FILE__, 'main_section');
+	register_setting( 'jcss_options', 'jcss_options', 'jcss_options_validate' );
+	add_settings_section( 'main_section', 'Main Settings', 'section_text_fn', __FILE__ );
+	add_settings_field( 'main_font', 'Google Font', 'setting_font_dropdown_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_nav', 'Navbar Colour', 'setting_nav_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_nav_col', 'Select menu color scheme', 'setting_nav_dropdown_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_widget', 'Widget Colour', 'setting_widget_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_sticky', 'Sticky Colour', 'setting_sticky_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_bpo_radio', 'Enable bypostauthor css', 'setting_bpo_enable_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_bypostauthor', 'Colour for .bypostauthor', 'setting_bypostauthor_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_comment_even', 'Comments even', 'setting_comment_even_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_comment_odd', 'Comments odd', 'setting_comment_odd_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_aside', 'Aside Colour', 'setting_aside_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_corners', 'Corner radius', 'setting_corner_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_brackets', 'Enable {} around blog title', 'setting_brackets_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_mainfont', 'Primary font colour', 'setting_mainfont_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_width', 'Theme width px', 'setting_width_fn', __FILE__, 'main_section' );
+	add_settings_field( 'jcss_reset', 'Restore Defaults?', 'setting_reset_fn', __FILE__, 'main_section' );
+
+	foreach( jcss_google_fonts() as $font ) {
+		wp_register_style( $font, 'http://fonts.googleapis.com/css?family=' . $font );
+		wp_enqueue_style( $font );
+		}
+}
+
+// Enable google font previews
+
+function jcss_google_fonts() {
+	$fonts = array("Molengo", "Cantarell", "Arimo", "puritan", "Crimson Text", "Droid Sans" );
+	return( apply_filters( 'jcss_google_fonts', $fonts ) );
 }
 
 // Add sub page to the Settings Menu
@@ -36,10 +48,10 @@ function options_page_fn() {
 		<div class="icon32" id="icon-options-general"><br></div>
 		<h2>JustCSS Options Page</h2>
 		<form action="options.php" method="post">
-		<?php settings_fields('jcss_options'); ?>
-		<?php do_settings_sections(__FILE__); ?>
+		<?php settings_fields( 'jcss_options' ); ?>
+		<?php do_settings_sections( __FILE__ ); ?>
 		<p class="submit">
-			<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Changes'); ?>" />
+			<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes' ); ?>" />
 		</p>
 		</form>
 	</div>
@@ -48,8 +60,8 @@ function options_page_fn() {
 
 // Validate user data for some/all of your input fields
 function jcss_options_validate($input) {
-	$input['corner'] = ( (int) $input['corner'] && $input['corner'] > 0 && $input['corner'] < 25 ) ? $input['corner'] : 8;
-	$input['width'] = ( (int) $input['width'] && $input['width'] > 720 && $input['width'] < 2000 ) ? $input['width'] : 1024;
+	$input['corner'] = ( ( int ) $input['corner'] && $input['corner'] > 0 && $input['corner'] < 25 ) ? $input['corner'] : 8;
+	$input['width'] = ( ( int ) $input['width'] && $input['width'] > 720 && $input['width'] < 2000 ) ? $input['width'] : 1024;
 	$input['nav'] = ( preg_match( '/^#?([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?$/', $input['nav'], $out ) ) ? $out[0] : 'eee';
 	$input['widget'] = ( preg_match( '/^#?([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?$/', $input['widget'], $out ) ) ? $out[0] : 'eee';
 	$input['sticky'] = ( preg_match( '/^#?([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?$/', $input['sticky'], $out ) ) ? $out[0] : 'eee';
@@ -75,10 +87,10 @@ function setting_nav_fn() {
 
 function  setting_nav_dropdown_fn() {
 	$options = get_option('jcss_options');
-	$items = array("JustCSS", "Toolbox");
+	$items = array( "JustCSS", "Toolbox" );
 	echo "<select id='nav_col' name='jcss_options[nav_col]'>";
-	foreach($items as $item) {
-		$selected = ($options['nav_col']==$item) ? 'selected="selected"' : '';
+	foreach( $items as $item ) {
+		$selected = ( $options['nav_col'] == $item ) ? 'selected="selected"' : '';
 		echo "<option value='$item' $selected>$item</option>";
 	}
 	echo "</select>";
@@ -86,11 +98,11 @@ function  setting_nav_dropdown_fn() {
 
 function  setting_font_dropdown_fn() {
 	$options = get_option('jcss_options');
-	$items = array("Molengo", "Cantarell", "Arimo", "puritan", "Crimson Text", "Droid Sans");
-	echo "<select id='main_font' name='jcss_options[main_font]'>";
+	$items = jcss_google_fonts();
+	echo "<select style='font-family: {$options['main_font']};font-size:14px' id='main_font' name='jcss_options[main_font]'>";
 	foreach( $items as $item ) {
-		$selected = ($options['main_font']==$item) ? 'selected="selected"' : '';
-		echo "<option value='$item' $selected>$item</option>";
+		$selected = ( $options['main_font'] == $item ) ? 'selected="selected"' : '';
+		echo "<option style='font-family: $item;font-size:18px' value='$item' $selected>$item</option>";
 	}
 	echo "</select>";
 }
@@ -112,9 +124,9 @@ function setting_sticky_fn() {
 
 function setting_bpo_enable_fn() {
 	$options = get_option('jcss_options');
-	$items = array("Yes", "No");
-	foreach($items as $item) {
-		$checked = ($options['bpo']==$item) ? ' checked="checked" ' : '';
+	$items = array( "Yes", "No" );
+	foreach( $items as $item ) {
+		$checked = ($options['bpo'] == $item ) ? ' checked="checked" ' : '';
 		echo "<label><input ".$checked." value='$item' name='jcss_options[bpo]' type='radio' /> $item</label><br />";
 	}
 }
@@ -146,9 +158,9 @@ function setting_corner_fn() {
 
 function setting_brackets_fn() {
 	$options = get_option('jcss_options');
-	$items = array("Yes", "No");
-	foreach($items as $item) {
-		$checked = ($options['brackets']==$item) ? ' checked="checked" ' : '';
+	$items = array( "Yes", "No" );
+	foreach( $items as $item ) {
+		$checked = ( $options['brackets'] == $item ) ? ' checked="checked" ' : '';
 		echo "<label><input ".$checked." value='$item' name='jcss_options[brackets]' type='radio' /> $item</label><br />";
 	}
 }
@@ -165,7 +177,7 @@ function setting_width_fn() {
 
 function setting_reset_fn() {
 	$options = get_option('jcss_options');
-	$checked = ( isset( $options['reset']) ) ? ' checked="checked" ' : '';
+	$checked = ( isset( $options['reset'] ) ) ? ' checked="checked" ' : '';
 
 
 //	if( isset( $options['reset']) ) { $checked = ' checked="checked" '; }
@@ -176,6 +188,6 @@ function add_defaults_fn() {
 	$tmp = get_option('jcss_options');
 	if ( ( isset( $tmp['reset'] ) && $tmp['reset'] == 'on' ) || ( !is_array( $tmp ) ) ) {
 		$arr = array("nav"=>"eee", "nav_col" => "JustCSS", "widget" => "eee", "sticky" => "eee", "bpo" => "No", "bypostauthor" => "eee", "even" => "eee", 'odd' => 'fcfcfc', 'aside' => 'f2f2f2', 'corner' => '8', 'brackets' => 'Yes', 'mainfont' => '000', 'width' => '1024', 'main_font' => 'Canterell');
-		update_option('jcss_options', $arr);
+		update_option( 'jcss_options', $arr );
 	}
 }
